f30e42dc0672c9c110b641c6a10bca3ecda76024
pierreadorni <pierreadorni@free.fr>
2022-01-04 17:58:15 +0100

    glowing star effect working

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,7 +1,18 @@
 import React from 'react'
+import logo from '../../../assets/logo.svg'
+import './home.css'
 
 function Home() {
-  return <h1> Ceci est la page d&apos;accueil !</h1>
+  return (
+    <div className="background">
+      <div className="star-glow-wrap">
+        <div className="star-glow" />
+        <div className="star-glow-wide" />
+      </div>
+
+      <img src={logo} alt="logo de compiègne en lumière" />
+    </div>
+  )
 }
 
 export default Home
