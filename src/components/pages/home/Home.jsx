f544f49d034b607eab076c2c83e6317c902a2ef6
pierreadorni <pierreadorni@free.fr>
2022-01-04 18:23:23 +0100

    landing page footer without link to scroll down

--- src/components/pages/home/Home.jsx
+++ src/components/pages/home/Home.jsx
@@ -1,5 +1,6 @@
 import React from 'react'
 import logo from '../../../assets/logo.svg'
+import arrow from '../../../assets/arrow.svg'
 import './home.css'
 
 function Home() {
@@ -11,6 +12,10 @@ function Home() {
       </div>
 
       <img src={logo} alt="logo de compiègne en lumière" />
+      <div className="section-footer">
+        <h2>Découvrez</h2>
+        <img src={arrow} alt="flèche vers le bas" />
+      </div>
     </div>
   )
 }
