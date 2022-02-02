2f8dea15cb99b6d0df7653ab708c7e8759790d97
pierre adorni <pierreadorni@free.fr>
2022-02-02 16:27:29 +0100

    added useEffect to call play() method of video tag

--- src/components/ui/atoms/JellyFishVideo/JellyFishVideo.jsx
+++ src/components/ui/atoms/JellyFishVideo/JellyFishVideo.jsx
@@ -1,8 +1,11 @@
-import React from 'react'
+import React, { useEffect } from 'react'
 import './jellyFishVideo.css'
 import jellyfish from '../../../../assets/jellyfish.mp4'
 
 function JellyFishVideo() {
+  useEffect(() => {
+    document.getElementsByClassName('jellyfish-video')[0].play()
+  }, [])
   return (
     <div className="jellyfish-video-frame">
       <video src={jellyfish} className="jellyfish-video" loop autoPlay muted>
