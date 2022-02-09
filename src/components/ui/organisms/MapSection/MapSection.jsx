e6488b73e28bf9acab88a234c017f147f5e1aa6f
DRJonshon <pierreadorni@free.fr>
2022-02-09 14:37:48 +0100

    map section with mock description & map

--- src/components/ui/organisms/MapSection/MapSection.jsx
+++ src/components/ui/organisms/MapSection/MapSection.jsx
@@ -1,11 +1,18 @@
 import React from 'react'
 import './mapSection.css'
+import LoremIpsum from 'react-lorem-ipsum'
 
 function MapSection() {
   return (
-    <div className="MapSection" id="map-section">
+    <div className="map-section" id="map-section">
       <div className="section-title">
-        <h1>L&#39;évènement</h1>
+        <h1>Le parcours</h1>
+      </div>
+      <div className="map-description">
+        <LoremIpsum avgSentencesPerParagraph={2} />
+      </div>
+      <div className="map-image">
+        <div className="fake-map" />
       </div>
     </div>
   )
