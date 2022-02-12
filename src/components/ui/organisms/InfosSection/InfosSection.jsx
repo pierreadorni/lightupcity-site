ba30173a83877d02c403f352175a169744cf746b
DRJonshon <pierreadorni@free.fr>
2022-02-12 17:11:40 +0100

    first version of InfosSection

--- src/components/ui/organisms/InfosSection/InfosSection.jsx
+++ src/components/ui/organisms/InfosSection/InfosSection.jsx
@@ -1,10 +1,49 @@
 import React from 'react'
+import { HiLocationMarker, HiCalendar } from 'react-icons/hi'
 import './infosSection.css'
 
 function InfosSection() {
   return (
     <div className="infos-section">
-      <h1> Infos Section </h1>
+      <div className="progressbar">
+        {[...Array(40)].map(() => (
+          <svg className="progressbar-stripe">
+            <defs>
+              <filter id="f1" x="0" y="0">
+                <feGaussianBlur in="SourceGraphic" stdDeviation="2" />
+              </filter>
+            </defs>
+            <polygon points="0,-10 10,-10 10,30 0,30" filter="url(#f1)" />
+          </svg>
+        ))}
+      </div>
+      <div className="infos-section-content">
+        <div className="info">
+          <HiLocationMarker size={40} />
+          <p>
+            Rue Roger Couttolenc,
+            <br /> Compiègne
+          </p>
+        </div>
+        <div className="info">
+          <HiCalendar size={40} />
+          <p>
+            Samedi 26 février, <br /> 20h
+          </p>
+        </div>
+      </div>
+      <div className="progressbar">
+        {[...Array(40)].map(() => (
+          <svg className="progressbar-stripe">
+            <defs>
+              <filter id="f1" x="0" y="0">
+                <feGaussianBlur in="SourceGraphic" stdDeviation="2" />
+              </filter>
+            </defs>
+            <polygon points="0,-10 10,-10 10,30 0,30" filter="url(#f1)" />
+          </svg>
+        ))}
+      </div>
     </div>
   )
 }
