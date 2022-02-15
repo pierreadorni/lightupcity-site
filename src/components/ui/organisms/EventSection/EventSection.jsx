1232e3fca1c8b3bacec77665ede9d5d1847a5c01
DRJonshon <pierreadorni@free.fr>
2022-02-15 16:34:33 +0100

    removed jellyfish video

--- src/components/ui/organisms/EventSection/EventSection.jsx
+++ src/components/ui/organisms/EventSection/EventSection.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 // import { LoremIpsum } from 'react-lorem-ipsum'
 import './eventSection.css'
-import JellyFishVideo from '../../atoms/JellyFishVideo/JellyFishVideo'
+// import JellyFishVideo from '../../atoms/JellyFishVideo/JellyFishVideo'
 
 function EventSection() {
   return (
@@ -10,27 +10,25 @@ function EventSection() {
         <h1>L&#39;évènement</h1>
       </div>
       <div className="event-section-content">
-        <div className="event-section-top">
-          <div className="event-description">
-            <p>
-              Cette année, le <b>samedi 26 février</b> aura lieu la 5ème édition
-              de Compiègne en Lumière, un parcours lumineux et musical organisé
-              par 50 étudiant·e·s motivé·e·s de l&#39; Université Technologique
-              de Compiègne.{' '}
-              <b>
-                Spectacles pyrotechniques, concerts, parcours lumineux, stands
-                photos, fresques
-              </b>{' '}
-              seront au rendez-vous. Comme chaque année, il nous tient à cœur de
-              réunir habitant·e·s, familles et étudiant·e·s, le temps d’une
-              soirée animée, sur le thème de la musique et de la lumière. Nous
-              vous attendons avec impatience !
-            </p>
-          </div>
-          <div className="jellyfish-video-container">
-            <JellyFishVideo />
-          </div>
+        <div className="event-description">
+          <p>
+            Cette année, le <b>samedi 26 février</b> aura lieu la 5ème édition
+            de Compiègne en Lumière, un parcours lumineux et musical organisé
+            par 50 étudiant·e·s motivé·e·s de l&#39; Université Technologique de
+            Compiègne.{' '}
+            <b>
+              Spectacles pyrotechniques, concerts, parcours lumineux, stands
+              photos, fresques
+            </b>{' '}
+            seront au rendez-vous. Comme chaque année, il nous tient à cœur de
+            réunir habitant·e·s, familles et étudiant·e·s, le temps d’une soirée
+            animée, sur le thème de la musique et de la lumière. Nous vous
+            attendons avec impatience !
+          </p>
         </div>
+        {/* <div className="jellyfish-video-container"> */}
+        {/*   <JellyFishVideo /> */}
+        {/* </div> */}
         <div className="event-section-bottom">
           <iframe
             className="aftermovie"
